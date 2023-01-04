5a1f53bc029989bee7ee6be6d81a5f52d58f4ca9
AmAn-2O <112274867+AmAn-2O@users.noreply.github.com>
2023-01-04 23:41:16 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+""" def fib(n):
+ first=0
+ second=1
+ print(first,second,end=" ")
+ for i in range(n-2):
+    next_no=first+second
+    print(next_no,end=" ")
+    first=second
+    second=next_no
+ """
+def leapyear(x):
+ if x%4==0 or x%100==0:
+    print(x,"is leap year")
+ else:
+    print(x,"Not a leap year.")
