0310f3ebdf6bd1983c7fd160fae410e3101d8077
AmAn-2O <112274867+AmAn-2O@users.noreply.github.com>
2023-01-05 22:58:49 +0530

    Add files via upload

---
+++ fin.py
@@ -0,0 +1,14 @@
+""" import fibonacci
+x=int(input("Enter the  no:"))
+print(fibonacci.fib(x)) """
+""" from fibonacci import *
+x=int(input("Enter the  no:"))
+print(fib(x))
+  """
+""" import fibonacci 
+x=int(input("Enter the No:"))
+print(fibonacci.leapyear(x))
+"""
+from fibonacci import *
+x=int(input("Enter the No:"))
+print(leapyear(x)) 
